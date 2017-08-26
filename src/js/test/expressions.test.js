cb79f1b9b8aabd5c73419c91d1c7ec9d99a4189c
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-08-26 18:20:55 +0300

    Added test cases for the expression backspace delete func

--- src/js/test/expressions.test.js
+++ src/js/test/expressions.test.js
@@ -27,3 +27,51 @@ describe('addDigitToExpression function', function() {
     }
   );
 });
+
+describe('deleteLastChar function', function() {
+  it('should return empty expression, when given empty expression',
+    function() {
+      expect(e.deleteLastChar('', '')).to.deep.equal(
+        {'left': '', 'right': ''}
+      );
+    }
+  );
+
+  it('should return empty expr if the given expr has a single char',
+    function() {
+      expect(e.deleteLastChar('4', '')).to.deep.equal(
+        {'left': '', 'right': ''}
+      );
+    }
+  );
+
+  it('should delete last character if it is a digit from left expr',
+    function() {
+      expect(e.deleteLastChar('(23+32', ')')).to.deep.equal(
+        {'left': '(23+3', 'right': ')'}
+      );
+    }
+  );
+
+  it('should delete last character if it is an operator from left expr',
+    function() {
+      expect(e.deleteLastChar('45-', '')).to.deep.equal(
+        {'left': '45', 'right': ''}
+      );
+    }
+  );
+
+  it('should move rightmost ")" from the left expr to the right one',
+    function() {
+      expect(e.deleteLastChar('34+((23+2)', ')')).to.deep.equal(
+        {'left': '34+((23+2', 'right': '))'}
+      );
+    }
+  );
+
+  it('should remove the bracket pair, when deleting a "("', function() {
+    expect(e.deleteLastChar('65+(23+45*(', '))')).to.deep.equal(
+      {'left': '65+(23+45*', 'right': ')'}
+    );
+  });
+});
